2dd337b4016ddcd5fc91cd344bfcfc7284d82ba5
Aleksey <115171964+nzrnk@users.noreply.github.com>
2025-04-02 17:23:49 +0300

    Update messages.js

--- src/data/messages.js
+++ src/data/messages.js
@@ -1,13 +1,13 @@
 const errors = {
     crushBug: 'You found a crash bug, examine the page by clicking on any button for 5 seconds',
     crushBugShort: 'You found a crash bug, examine the page for 5 seconds'
-}
+};
 
 
 const messages = {
     firstCongrats: '#1 Awesome! You found a bug. Pretty easy right?',
     bugPopUpMessage: 'What did you find out?',
-}
+};
 
 
 
